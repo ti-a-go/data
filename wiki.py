a6130daa342380fc0b96920a9154e40c6c95cf97
--add <tiagosn06@gmail.com>
2023-10-16 22:11:59 -0300

    add logs when downloading and saving the pages

--- wiki.py
+++ wiki.py
@@ -9,7 +9,8 @@ def save_summaries(pages):
     if not os.path.exists(SUMMARIES_FOLEDER):
         os.makedirs(SUMMARIES_FOLEDER)
     for page in pages:
-        file_name = page.title.replace(" ", "_") + ".txt" 
+        file_name = page.title.replace(" ", "_") + ".txt"
+        print(f"Saving page - {page.title}")
         with open(os.path.join(SUMMARIES_FOLEDER, file_name), "w") as file:
             file.write(page.summary)
             file.close()
@@ -17,10 +18,14 @@ def save_summaries(pages):
 def search_and_save_summaries(query):
     wikipedia.set_lang("pt")
     results = wikipedia.search(query)
-    pages = [wikipedia.page(result) for result in results]
+    pages = []
+    for result in results:
+        print(f"Downloading page - {result}")
+        pages.append(wikipedia.page(result))
+    # pages = [wikipedia.page(result) for result in results]
     save_summaries(pages)
 
 
 if __name__ == "__main__":
-    query = "historia negra"
-    search_and_save_summaries(query)
+    # This function search on Wikipedia an saves the summaries from all pages returned from the search.
+    search_and_save_summaries("premissa")
